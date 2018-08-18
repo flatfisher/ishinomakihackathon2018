4765fd8c6fa1cc0f59cc6259e940b7dc3e930283
flatfisher <shimano.entou@gmail.com>
2018-08-18 11:25:20 +0900

    Add an ARCore

--- android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/MainActivity.kt
+++ android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/MainActivity.kt
@@ -1,12 +1,45 @@
 package jp.flatfish.ishinomakihack2018
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java!!.getSimpleName()
+    private val MIN_OPENGL_VERSION = 3.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return
+        }
+
         setContentView(R.layout.activity_main)
+    }
+
+    fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later")
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show()
+            activity.finish()
+            return false
+        }
+        val openGlVersionString = (activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+                .deviceConfigurationInfo
+                .glEsVersion
+        if (java.lang.Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show()
+            activity.finish()
+            return false
+        }
+        return true
     }
 }
