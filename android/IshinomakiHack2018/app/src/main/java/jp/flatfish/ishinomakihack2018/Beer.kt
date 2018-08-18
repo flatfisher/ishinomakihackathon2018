4ffeee9852e3428e33c7cac9ee3261fef96490a9
to <>
2018-08-18 18:41:20 +0900

    confirm

--- android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/Beer.kt
+++ android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/Beer.kt
@@ -2,9 +2,29 @@ package jp.flatfish.ishinomakihack2018
 
 import com.google.firebase.firestore.GeoPoint
 
+import android.location.Location
+
 class Beer {
     var name: String? = null
     var msg: String? = null
     var location: GeoPoint? = null
     var tags:Array<String>? = null
+    var distance: Float? = null
+
+    fun calcDist(latitude: Double, longitude: Double) {
+        var results = FloatArray(3)
+        location?.let {
+            Location.distanceBetween(it.latitude, it.longitude, latitude, longitude, results)
+            distance = results[0]
+        }
+    }
+
+    fun checkTag(tag: String): Beer? {
+        tags?.forEach {
+            if (it == tag) {
+                return this
+            }
+        }
+        return null
+    }
 }
--- android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/MainActivity.kt
+++ android/IshinomakiHack2018/app/src/main/java/jp/flatfish/ishinomakihack2018/MainActivity.kt
@@ -9,12 +9,31 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import android.support.v4.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.Manifest
+import android.location.LocationManager
+import android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS
+import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.support.annotation.NonNull
+import android.location.LocationProvider
+import android.provider.Settings
+import java.util.stream.Collectors
 
 
-class MainActivity : AppCompatActivity() {
-    private val TAG = MainActivity::class.java!!.getSimpleName()
+class MainActivity : AppCompatActivity(), LocationListener {
+    private val TAG = MainActivity::class.java.simpleName
+
     private val MIN_OPENGL_VERSION = 3.0
     private var mBeers = mutableListOf<Beer>()
+    private var sortedBeers = mutableListOf<Beer>()
+
+    private var nowLocation: Location? = null
+
+    private var locationManager: LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +41,19 @@ class MainActivity : AppCompatActivity() {
             return
         }
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1000)
+        } else {
+            locationStart()
+
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 10f, this)
+
+        }
 
         //Firebase
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
@@ -43,9 +75,47 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
 
+
+    }
+    private fun locationStart() {
+        Log.d("debug", "locationStart()")
+
+        // LocationManager インスタンス生成
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager != null && locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled")
+        } else {
+            // GPSを設定するように促す
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+
+            Log.d("debug", "checkSelfPermission false")
+            return
+        }
+
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 50f, this)
+
     }
 
-    fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        when (status) {
+            LocationProvider.AVAILABLE -> Log.d("debug", "LocationProvider.AVAILABLE")
+            LocationProvider.OUT_OF_SERVICE -> Log.d("debug", "LocationProvider.OUT_OF_SERVICE")
+            LocationProvider.TEMPORARILY_UNAVAILABLE -> Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    private fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
             Log.e(TAG, "Sceneform requires Android N or later")
             Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show()
@@ -63,5 +133,67 @@ class MainActivity : AppCompatActivity() {
             return false
         }
         return true
+    }
+
+    private fun sort(beers: List<Beer>): List<Beer> {
+        return beers.stream().sorted { beer1, beer2 ->
+            if(beer1.distance == null || beer2.distance == null) {
+                return@sorted 0
+            } else if (beer1.distance!! > beer2.distance!!) {
+                return@sorted -1
+            } else {
+                return@sorted 1
+            }
+        }.collect(Collectors.toList())
+    }
+
+    private fun calcDistance() {
+        mBeers.forEach { beer ->
+            nowLocation?.let {
+                beer.calcDist(it.latitude, it.longitude)
+            }
+        }
+    }
+
+    private fun getBeerEstmate(tag: String): Beer? {
+        sortedBeers?.forEach { beer ->
+            beer.checkTag(tag)?.let {
+                return it
+            }
+        }
+        return null
+    }
+
+    override fun onLocationChanged(location: Location) {
+        nowLocation = location
+        Log.d("debug", "Latitude: ${location.latitude}")
+        Log.d("debug", "Longitude: ${location.longitude}")
+
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug", "checkSelfPermission true")
+
+                locationStart()
+
+            } else {
+                // それでも拒否された時の対応
+                val toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
