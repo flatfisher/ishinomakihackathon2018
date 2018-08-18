00d549467d5adf6950bfbf639b6481362fec73e9
brown <joanh4mjst@gmail.com>
2018-08-18 10:58:11 +0900

    init project

---
+++ web/src/components/entry/Index.vue
@@ -0,0 +1,44 @@
+<template lang='pug'>
+.vue-index
+    .reactive-title {{ reactiveTitle() }}
+    common-navbar.is-light(:brand='title')
+    common-hero.is-dark(:title='title' :subtitle='subtitle')
+
+    section.main.section
+        sample
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import Buefy from 'buefy';
+import RootVue from '@/components/base/RootVue';
+
+import CommonNavbar from '@/components/common/CommonNavbar.vue';
+import CommonHero from '@/components/common/CommonHero.vue';
+import Sample from '@/components/sample/Sample.vue';
+
+Vue.use(Buefy);
+
+/**
+ * Vue Component
+ */
+@Component({
+    components: {
+        CommonNavbar, CommonHero, Sample
+    }
+})
+export default class Index extends RootVue {
+    public title = 'Sample';
+    public subtitle = 'Index';
+}
+</script>
+
+<style lang='sass'>
+@import 'entry/all'
+
+.vue-index
+    section.main
+        max-width: 800px
+        margin-left: auto
+        margin-right: auto
+</style>
---
+++ web/src/components/sample/SampleModal.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+section.vue-sample-modal.section
+    h3.title ログイン
+    form.form
+        b-field(label='Email')
+            b-input(type='text')
+        b-field(label='Password')
+            b-input(type='password')
+
+        .buttons.is-right
+            a.button.is-primary ログイン
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+
+/**
+ * Vue Component
+ */
+@Component
+export default class SampleModal extends Vue {
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'entry/variable'
+
+.vue-sample-modal
+    background: $white
+    border-radius: 4px
+</style>
